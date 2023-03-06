3496010159da0315ce86026dd824940542ada520
zewdu erkyhun <zewdu444@gmail.com>
2023-03-06 22:57:23 +0300

    Add reserved mission in myprofile

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
+import Badge from 'react-bootstrap/Badge';
 import { fetchMissions, joinMission } from '../redux/missions/missionsSlice';
 
 function Missions() {
   const dispatch = useDispatch();
   const status = useSelector((state) => state.missions.status);
   const missions = useSelector((state) => state.missions.missionstore);
-  // const [join] = useState('Join Misson');
   useEffect(() => {
     if (status === 'idle') {
       dispatch(fetchMissions());
@@ -19,7 +19,7 @@ function Missions() {
     dispatch(joinMission(e.target.id));
   };
   return (
-    <div className="px-4 pr-4">
+    <div className="px-5 pr-5">
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -33,16 +33,16 @@ function Missions() {
           {
           missions.map((mission) => (
             <tr key={mission.mission_id}>
-              <td className="fw-bold">{mission.mission_name}</td>
-              <td className="w-50">
+              <td className="fw-bold " style={{ width: '15%' }}>{mission.mission_name}</td>
+              <td style={{ width: '45%' }}>
                 {mission.description}
               </td>
-              <td className="text-center align-middle">
-                <Button variant={mission.reserved ? ('success') : ('secondary')} size="sm" disabled>
+              <td className="text-center align-middle" style={{ width: '10%' }}>
+                <Badge bg={mission.reserved ? ('success') : ('secondary')}>
                   {mission.reserved ? ('Active Member') : ('NOT A MEMBER')}
-                </Button>
+                </Badge>
               </td>
-              <td className="text-center align-middle">
+              <td className="text-center align-middle" style={{ width: '15%' }}>
                 <Button
                   id={mission.mission_id}
                   variant={mission.reserved ? ('outline-danger') : ('outline-secondary')}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,18 +7,29 @@ import logo from '../asset/planet.png';
 function NavBar() {
   return (
     <div>
-      <Navbar bg="white" expand="lg" className="px-4">
+      <Navbar bg="white" expand="lg" className="px-5">
         <Navbar.Brand to="/" as={Link} className="fs-2 fw-medium">
           <img alt="planet log" src={logo} width={55} height={55} />
-          {' '}
+          {'  '}
           Space Traveler&#39;s Hub
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav " />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end px-5">
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-end"
+        >
           <Nav className="text-primary fs-5">
-            <Nav.Link className="text-primary pr-5" to="/" as={Link}>Rockets</Nav.Link>
+            <Nav.Link
+              className="text-primary pe-5"
+              to="/"
+              activeClassName="underline"
+              as={Link}
+            >
+              Rockets
+
+            </Nav.Link>
             <Nav.Link className="text-primary" to="/missions" as={Link}>Missions</Nav.Link>
-            <div className="d-none d-md-none d-lg-block  vr" />
+            <div className="d-none d-md-none d-lg-block mt-3 mb-3 vr" />
             <Nav.Link className="text-primary" to="/myprofile" as={Link}>My profile</Nav.Link>
           </Nav>
         </Navbar.Collapse>
