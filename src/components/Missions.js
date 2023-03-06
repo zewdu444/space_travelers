12456414672a1f22292869cf3da1f18933be1ed1
zewdu erkyhun <zewdu444@gmail.com>
2023-03-06 15:49:48 +0300

    Add mock data to table

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 function Missions() {
   return (
-    <div>Missions</div>
+    <div className="px-4 pr-4">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>{}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="fw-bold">Thaicom</td>
+            <td className="w-50">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              In quibusdam sed exercitationem consequuntur quaerat.
+              Facilis laudantium ut minima quaerat soluta, aliquid mollitia
+              voluptas at blanditiis vero architecto assumenda aut incidunt?
+            </td>
+            <td className="text-center align-middle">
+              <Button variant="secondary" size="sm" disabled>
+                NOT A MEMBER
+              </Button>
+            </td>
+            <td className="text-center align-middle">
+              <Button variant="outline-secondary" size="md">
+                Join Mission
+              </Button>
+            </td>
+          </tr>
+          <tr>
+            <td className="fw-bold">Telstar</td>
+            <td className="w-50">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              In quibusdam sed exercitationem consequuntur quaerat.
+              Facilis laudantium ut minima quaerat soluta, aliquid mollitia
+              voluptas at blanditiis vero architecto assumenda aut incidunt?
+            </td>
+            <td className="text-center align-middle">
+              <Button variant="success" size="sm" disabled>
+                NOT A MEMBER
+              </Button>
+            </td>
+            <td className="text-center align-middle">
+              <Button variant="outline-danger align-middle" size="md">
+                Join Mission
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
+    </div>
   );
 }
 
