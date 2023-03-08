d1702e22dca74064cb1e564deb7b6e196ebbeed7
Donmark2k <chukwuemekavp@gmail.com>
2023-03-08 14:13:43 +0100

    Import axios

--- src/redux/rockets/RocketsSlice.js
+++ src/redux/rockets/RocketsSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const api = 'https://api.spacexdata.com/v3/missions';
 
@@ -7,6 +8,15 @@ const initialState = {
   status: 'idle',
   error: null,
 };
+
+export const fetchRockets = createAsyncThunk('missions/fetchMissions', async () => {
+  try {
+    const response = await axios.get(api);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 export const rocketsSlice = createSlice({
   name: 'rockets',
