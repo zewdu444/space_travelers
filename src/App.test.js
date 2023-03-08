354903b9d6311c43f5ac2be3d9250fb04243b511
zewdu erkyhun <zewdu444@gmail.com>
2023-03-08 21:50:53 +0300

    Add tests for mission component and slicer

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,81 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import NavBar from './components/NavBar';
+import Missions from './components/Missions';
+import store from './redux/store';
+import MyProfile from './components/MyProfile';
 import App from './App';
+import { fetchMissions, joinMission } from './redux/missions/MissionsSlice';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('App renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('NavBar page renders correctly', () => {
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Missons page renders correctly', async () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('MyProfile page renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <MyProfile />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+// mission slicer and reducer check
+
+describe('Mission redux state tests', () => {
+  it('Should initially set Missions store to an empty Array', () => {
+    const state = store.getState().missions;
+    expect(state.missionstore).toEqual([]);
+  });
+
+  it('should Join Mission payload send correct', () => {
+    const expectedPayload = { payload: '9D1B7E0', type: 'missions/joinMission' };
+    const actualPayload = joinMission('9D1B7E0');
+    expect(actualPayload).toEqual(expectedPayload);
+  });
+
+  it('Mission fetch data from API', async () => {
+    const url = 'https://api.spacexdata.com/v3/missions';
+    const axiosSpy = jest.spyOn(axios, 'get');
+    jest.setTimeout(90000);
+    const dispatchSpy = jest.fn();
+
+    await fetchMissions(url)(dispatchSpy);
+
+    expect(axiosSpy).toHaveBeenCalledWith(url);
+  });
 });
